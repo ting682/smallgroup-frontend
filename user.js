bdcbb56ff192cf74de64c391e0dd894e011e6793
ting682 <tchung682@gmail.com>
2020-10-21 13:43:48 -0400

    render signup

--- user.js
+++ user.js
@@ -5,7 +5,23 @@ class User {
         this.passages = passages
     }
 
-    renderPassagesForm() {
-        
+    static renderSignup() {
+        document.getElementById('modal').className = 'modal-active'
+
+        document.getElementsByClassName('modal-card-body')[0].innerHTML = ""
+
+        document.getElementsByClassName('modal-card-body')[0].innerHTML = 
+            `<form>
+                <label>Email</label><br>
+                <input type=\"text\" id=\"signup email\"></input><br><br>
+
+                <label>Password</label><br>
+                <input type=\"password\" id=\"signup password\"></input><br><br>
+
+                <label>Password confirmation<br>
+                <input type=\"password\" id=\"signup password confirmation\"></input><br><br>
+
+                <input type=\"submit\" id=\"signup\"></input>
+            </form>`
     }
 }
